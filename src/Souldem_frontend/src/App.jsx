1f497d526876d9b6bdb421012539f9e756535fda
electro7 <electroseven@gmail.com>
2024-03-19 03:49:33 -0400

    something happenend I don't know what

--- src/Souldem_frontend/src/App.jsx
+++ src/Souldem_frontend/src/App.jsx
@@ -15,8 +15,43 @@ function App() {
   }
 
   return (
-    <main>
-      <Navbar />
+    <main>  <body>
+    <div class="container">
+      <section aria-label="navbar">
+        <nav class="navbar">
+          <div class="align-left">
+            <div>
+              <img src="./assets/white-logo.png" alt="logo" class="logo" />
+            </div>
+            <div class="line"></div>
+            <div class="grow-list">
+              <ul class="list">
+                <li class="list-item">Apps</li>
+                <li class="list-item">DAO</li>
+                <li class="list-item">Technology</li>
+                <li class="list-item">Token</li>
+                <li class="list-item">Contributors</li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <button class="button-small">Buy 1 Token</button>
+          </div>
+        </nav>
+      </section>
+      
+      <section class="hero-section" aria-label="hero">
+        <div class="hero">
+          <h1 class="hero-heading">Lets build a better tomorrow, together.</h1>
+          <p class="hero-description">
+            Buy 1 Token. Join the DAO. Change the world
+          </p>
+          <button class="button-large">Request Early Access</button>
+        </div>
+      </section>
+    </div>
+  </body>
+
     </main>
   );
 }
